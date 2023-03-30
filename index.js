2f77ca86428b7ce1cf9b38cfa09f8521902e1823
james arasim <jarasim@loandepot.com>
2023-03-30 16:52:53 -0700

    solution using express

--- index.js
+++ index.js
@@ -4,37 +4,45 @@ import { parseFile } from 'music-metadata';
 import {createServer} from 'https';
 import {promises, readFileSync} from 'fs';
 import {join, extname} from 'path';
+import * as url from 'url';
+import express from 'express';
 
-let options = {key: readFileSync('./ssl/server.key'),
+const app = express();
+const port = 8080;
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const options = {key: readFileSync('./ssl/server.key'),
     cert: readFileSync('./ssl/server.cert')}
+const directoryPath = "./music";
 
-let directoryPath = "/Users/jameskennetharasim/Downloads/installed/static_volatile/STATIC_MUSIC/STATIC_MUSIC/_VINYL";
-createServer(options, async (req, res) => {
-    if (req.url === '/') {
-        try {
-            const files = await promises.readdir(directoryPath);
-            let fileNames = '';
-            for (const file of files) {
-                const filePath = join(directoryPath, file);
-                const stats = await promises.stat(filePath);
-                if (stats.isFile() && extname(filePath).toLowerCase() === '.mp3') {
-                    const metadata = await parseFile(filePath);
-                    fileNames += `<em style="color: limegreen">${file}</em> 
+//make files available in music subdirectory
+app.use('/music', express.static(join(__dirname, directoryPath.substring(2))));
+app.get('/', async (req,res) =>{
+    try {
+        const files = await promises.readdir(directoryPath);
+        let fileNames = '';
+        for (const file of files) {
+            const filePath = join(directoryPath, file);
+            const stats = await promises.stat(filePath);
+            if (stats.isFile() && extname(filePath).toLowerCase() === '.mp3') {
+                const metadata = await parseFile(filePath);
+                fileNames += `<a href="music/${file}" target="_blank" style="color: limegreen">${file}</a> 
                     Artist: ${metadata.common.artist}, 
                     Album: ${metadata.common.album}, 
                     Song: ${metadata.common.title}<br />`;
-                }
             }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(fileNames);
-        } catch (err) {
-            console.error(err);
-            res.writeHead(500);
-            res.end('Internal Server Error');
         }
-    } else {
-        res.writeHead(404);
-        res.end();
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(fileNames);
+    } catch (err) {
+        console.error(err);
+        res.writeHead(500);
+        res.end('Internal Server Error');
     }
-}).listen(8080);
+});
+
+createServer(options, app).listen(port, () => {
+    console.log(`Server listening on https://localhost:${port}`);
+});
+
 console.log("Server running at https://localhost:8080/");
+
